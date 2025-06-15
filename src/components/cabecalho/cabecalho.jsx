a031ed447bc9faee6ae40ddd3756f93ac7395d15
pedrozand <pedroliveira.eear@gmail.com>
2025-06-15 14:08:15 -0300

    Ajuste de cabecalho e linha

--- src/components/cabecalho/cabecalho.jsx
+++ src/components/cabecalho/cabecalho.jsx
@@ -6,22 +6,25 @@ const Cabecalho = () => {
   const { location } = useContext(LocationContext);
 
   return (
-    <div className="cabecalho-container">
-      <h1 className="cabecalho-titulo">Achados e Perdidos</h1>
-      <p className="cabecalho-subtitulo">
-        Veja os posts de pets próximos ao endereço:
-      </p>
-      <div className="cabecalho-barra-container">
-        <i className="fas fa-map-marker-alt icone-localizacao" />
-        <input
-          type="text"
-          value={location || ""}
-          readOnly
-          className="cabecalho-barra"
-          placeholder="Localização não disponível"
-        />
+    <>
+      <div className="cabecalho-container">
+        <h1 className="cabecalho-titulo">Achados e Perdidos</h1>
+        <p className="cabecalho-subtitulo">
+          Veja os posts de pets próximos ao endereço:
+        </p>
+        <div className="cabecalho-barra-container">
+          <i className="fas fa-map-marker-alt icone-localizacao" />
+          <input
+            type="text"
+            value={location || ""}
+            readOnly
+            className="cabecalho-barra"
+            placeholder="Localização não disponível"
+          />
+        </div>
       </div>
-    </div>
+      <div className="linha-cabecalho"></div>
+    </>
   );
 };
 
